beaaaabbb013d19c8fd908da9c1da9602502be64
osajab10@gmail.com <osajab10@gmail.com>
2022-09-16 17:48:05 +0200

    Add project files.

---
+++ LumberjacksAndFlapjacks/Lumberjack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumberjacksAndFlapjacks
+{
+    class Lumberjack
+    {
+        public string Name { get; private set; }
+        Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+
+        public Lumberjack(string name)
+        {
+            this.Name = name;
+        }
+
+        public void TakeFlapjack(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+
+        public void EatFlapjacks()
+        {
+            Console.WriteLine($"{Name} is eating flapjacks");
+            while(flapjackStack.Count > 0)
+            {
+                Console.WriteLine($"{Name} ate a {flapjackStack.Pop().ToString().ToLower()} flapjack");
+            }
+        }
+    }
+}
